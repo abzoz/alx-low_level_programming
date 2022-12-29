1126df825d9743052eadcc89321c051fa5c2c30f
mouthafar <mouthafar222@gmail.com>
2022-12-29 02:28:24 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stadio.h>
+
+
+/**
+ * main - print "Programming is like building a multilingual puzzle, followed by a new line."
+ * return 0.
+ */
+
+int main (void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle, followed by a new line.");
+
+	return (0);
+	
+}
