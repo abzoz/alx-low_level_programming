3f7ada3ecde6005be13bfdcc26985f00317e7ca6
mouthafar <mouthafar222@gmail.com>
2022-12-29 03:25:29 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,14 +5,14 @@
  * main - print "Programming is like building a multilingual
  * puzzle, followed by anew line.
  * Retrun: Alwayes 0.
- */
+ */                            
+
 
 int main (void)
 
-{
 
+{ 
 	puts("\"Programming is like building a multilingual puzzle");
+	retrun (0);
 
-	return (0);
-	
-}
+}	
