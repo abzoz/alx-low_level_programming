48bcfe490d187483a95996eeab898de5ab26c022
mouthafar <mouthafar222@gmail.com>
2022-12-29 15:58:02 -0800

    tf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints " with proper grammar, but the outcome is apiece of art,"followed by anew line.
+ * Return: Always 0.
+ *
+ */
+
+{
+	printf("with proper grammar, but the outcome is apiece of art,\n");
+	return (0);
+}	
